8583d750a85cee570450ebcde508a68ae5061bb3
t.he <study.hetao@foxmail.com>
2022-06-27 15:17:36 +0800

    excel转SQL

--- business/src/main/java/com/znsd/oneself/service/ExcelService.java
+++ business/src/main/java/com/znsd/oneself/service/ExcelService.java
@@ -1,15 +1,19 @@
 package com.znsd.oneself.service;
 
+import cn.hutool.core.io.IoUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
-import com.alibaba.excel.metadata.data.CellData;
 import com.alibaba.excel.metadata.data.ReadCellData;
 import com.alibaba.excel.read.metadata.ReadSheet;
 import com.alibaba.excel.util.ConverterUtils;
+import com.alibaba.excel.util.DateUtils;
 import com.google.common.collect.Maps;
 import com.znsd.oneself.message.Result;
+import com.znsd.oneself.utils.HttpUtils;
+import liquibase.util.FileUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jsqlparser.expression.StringValue;
 import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
@@ -18,10 +22,19 @@ import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.insert.Insert;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -36,25 +49,46 @@ import java.util.stream.Stream;
  */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ExcelService {
-    public Result<?> generateSql(MultipartFile multipartFile, boolean generateTable, Integer headRow) throws IOException {
+    private final HttpServletResponse httpServletResponse;
+
+    public Result<?> generateSql(MultipartFile multipartFile, boolean generateTable, Integer headRow,boolean export) throws IOException {
         if (generateTable) {
             //todo 生成临时表
         }
         final InputStream inputStream = multipartFile.getInputStream();
         Map<String,String> result = Maps.newHashMap();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("EXCEL转SQL任务");
         try(ExcelReader excelReader = EasyExcel.read(inputStream, getListener(result)).build();) {
             final List<ReadSheet> readSheets = excelReader.excelExecutor().sheetList();
             for (ReadSheet readSheet : readSheets) {
                 readSheet.setHeadRowNumber(headRow);
-                //解析
                 excelReader.read(readSheet);
             }
         }
-        return Result.success(result);
+        stopWatch.stop();
+        log.debug("耗时:{}",stopWatch.prettyPrint());
+        StringBuffer sb = new StringBuffer();
+        result.forEach((k,v) -> sb.append("#").append(k).append("\r\n").append(v));
+        final String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) +  multipartFile.getOriginalFilename() + ".sql";
+        //弹出下载
+        if (export) {
+            popWeb(sb.toString(), fileName);
+            return Result.success(Void.class);
+        }
+        return Result.success(sb);
     }
 
-
+    private void popWeb(String sb, String fileName) throws IOException {
+        try (OutputStreamWriter writer = IoUtil.getWriter(httpServletResponse.getOutputStream(), StandardCharsets.UTF_8)) {
+            httpServletResponse.setContentType("application/x-download;charset=utf-8");
+            httpServletResponse.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(fileName,
+                    StandardCharsets.UTF_8.toString()));
+            writer.append(sb);
+        }
+    }
 
 
     private AnalysisEventListener<Map<String, Object>> getListener(Map<String, String> result) {
@@ -96,7 +130,8 @@ public class ExcelService {
                 final MultiExpressionList multiExpressionList = new MultiExpressionList();
                 List<ExpressionList> expressionLists = new ArrayList<>();
                 for (List<Object> value : values) {
-                    final List<StringValue> stringValues = value.stream().map(Object::toString).map(StringValue::new).collect(Collectors.toList());
+                    final List<StringValue> stringValues =
+                            value.stream().map(item -> new StringValue(item == null ? "" : item.toString())).collect(Collectors.toList());
                     ExpressionList expressionList = new ExpressionList();
                     expressionList.addExpressions(stringValues);
                     expressionLists.add(expressionList);
@@ -105,7 +140,6 @@ public class ExcelService {
                 insert.setItemsList(multiExpressionList);
                 // 设置插入值
                 insert.setItemsList(multiExpressionList);
-                log.info("SQL:{}",insert.toString());
                 result.put(table,insert.toString());
             }
 
