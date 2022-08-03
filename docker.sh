2b41ba24ef0a2a1d703bddadff26347e8252eb96
t.he <study.hetao@foxmail.com>
2022-08-03 14:57:09 +0800

    jenkins测试

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
 build_date=`date +%m%d%H`
 echo "build_date 月日=${build_date}"
 project_tag="${brancher}_${build_date}"
 echo "project_tag=${brancher}"
 docker_imageinfo= "${CONTAINER}:${project_tag}"
+echo "docker_imageinfo=${docker_imageinfo}"
 #强制停止
 #docker stop `docker ps -a | grep -w ${CONTAINER} | awk '{print $1}'`
 # 删除滚动更新残留的容器
@@ -14,4 +14,4 @@ docker_imageinfo= "${CONTAINER}:${project_tag}"
 # 创建新镜像
 docker build -t ${docker_imageinfo} .
 
-docker run -d -p 30001:30001 --name=${CONTAINER} --network my-net ${docker_imageinfo}
+docker run -d -p 30001:30001 --name=${CONTAINER} ${docker_imageinfo}
